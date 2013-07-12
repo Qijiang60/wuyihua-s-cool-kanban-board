aac423e639ac03ead0a587292b26f146ae2c7355
Charles Ferentchak <ferentchak@gmail.com>
2013-07-11 22:22:53 -0400

    combo firing

--- App.js
+++ App.js
@@ -3,17 +3,39 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     //This tells the name for a css class added to the div that makes up the basic container for the app
     componentCls: 'app',
-
+    
+    cardboard:null,
+    
     launch: function() {
         var cardBoardConfig = {
             //This is defining what kind of thing I want to add to the App
             xtype: 'rallycardboard',
             //This is specifying what kinds of Rally Objects I want to display
-            types: ['Task'],
+            types: ['HierarchicalRequirement'],
             //This chooses which field on the types I want to use as the columns for the cardboard
-            attribute: "State"
+            attribute: "ScheduleState",
+            storeConfig:{
+                filters:[]
+            }
         };
-
-        this.add(cardBoardConfig);
+        this.add({
+            xtype: 'rallyattributecombobox',
+            model: 'UserStory',
+            field: 'Type',
+            defaultSelectionToFirst:true,
+            listeners:{
+                scope:this,
+                ready:function(){
+                    this.cardboard = this.add(cardBoardConfig);
+                },
+                change:function(){
+                    if(this.cardboard){
+                        this.cardboard.destroy();
+                    }
+                    this.cardboard = this.add(cardBoardConfig);
+                }
+            },
+        });
+        
     }
 });
